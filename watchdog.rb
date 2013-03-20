8afa770bb7d1af94ab60d4442e8385c0852292d9
William Blodgett <vigilant@cipherpunk.com>
2013-03-20 08:51:51 -0700

    Added hostname and service stamping to watchdog

--- watchdog.rb
+++ watchdog.rb
@@ -1,23 +1,25 @@
 #!/usr/bin/ruby
 
 require 'net/http'
-require "uri"
+require 'uri'
 require 'net/smtp'
+require 'socket'
 
-url = "http://somedomain/someurl"
-restart = "service your.service.name.here restart"
+check_url = "http://somedomain/someurl"
+expect_content = "foo"
+service_name = "your.service.name.here"
 mailserver = "your.smtp.server"
 from = "alertbot@example.com"
 to = "alertlist@example.com"
-message = "Subject:  Service is down!"
+message = "Subject: Service " + service_name + " on " + Socket.gethostname + " is down.  Restarting."
 
 smtp = Net::SMTP.new mailserver, 25 
 	
-Net::HTTP.get_response(URI.parse(url)) do |response|
+Net::HTTP.get_response(URI.parse(check_url)) do |response|
   response=response.body
-  if response.include?('foo')
+  if response.include?(expect_content)
   else 
-       system(restart) 
+       system("service", service_name, "restart")
        smtp.send_message(message, from, to) 
        smtp.finish	
   end
